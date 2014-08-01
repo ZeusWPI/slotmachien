59c8be9a629e5ac08736518485f2acb7a81119de
Jeroen <jeroen.declerck@ugent.be>
2014-08-01 18:20:14 +0200

    fixed indentations, improved NXTMain

--- SlotMachienNXT/src/NXTMain.java
+++ SlotMachienNXT/src/NXTMain.java
@@ -3,61 +3,67 @@ import lejos.nxt.*;
 import lejos.nxt.comm.*;
 
 public class NXTMain {
-	public static void main(String[] args) throws Exception {
-		boolean quit = true;
-		while (quit) {
 
-			LCD.drawString("Waiting", 0, 0);
-			NXTConnection connection = USB.waitForConnection();
-			DataInputStream dis = connection.openDataInputStream();
+    public static void main(String[] args) throws Exception {
 
-			LCD.clear();
-			LCD.drawString("Connected", 0, 0);
+        boolean quit = false;
+        while (!quit) {
 
-			boolean stop = true; 
-			
-			while (stop) {
-				/**
-				 * als 1 gestuurd wordt dan draaien de motoren een halve slag vooruit
-				 * als 2 gestuurd wordt dan draaien de motoren een halve slag achteruit
-				 * als 3 gestuurd wordt dan herstart het programma
-				 * als 4 (of groter) gestuurd wordt dan stopt het programma
-				 */
+            LCD.drawString("Waiting", 0, 0);
+            NXTConnection connection = USB.waitForConnection();
+            DataInputStream dis = connection.openDataInputStream();
 
-				byte b = dis.readByte(); 
-				if (b > 2) {
-					stop = false;
-				}
-				if (b > 3) {
-					quit = false;
-				}
-				turn(b == 1);
-			}
+            LCD.clear();
+            LCD.drawString("Connected", 0, 0);
 
-			dis.close();
+            boolean restart = false;
 
-			LCD.clear();
-			LCD.drawString("Closing", 0, 0);
+            while (!restart) {
+                /**
+                 * als 1 gestuurd wordt dan draaien de motoren een halve slag
+                 * vooruit als 2 gestuurd wordt dan draaien de motoren een halve
+                 * slag achteruit als 3 gestuurd wordt dan herstart het
+                 * programma als 4 (of groter) gestuurd wordt dan stopt het
+                 * programma
+                 */
 
-			Thread.sleep(1000);
+                byte b = dis.readByte();
+                if (b > 2) {
+                    restart = true;
+                    LCD.clear();
+                    LCD.drawString("Restarting", 0, 0);
 
-			connection.close();
-			LCD.clear();
-		}
-	}
+                    if (b > 3) {
+                        quit = true;
+                        LCD.clear();
+                        LCD.drawString("Quiting", 0, 0);
+                    }
+                } else {
+                    turn(b == 1);
+                }
+            }
 
-	public static void turn(boolean b) {
-		int turn = 180;
-		if (!b) {
-			turn = -turn;
-		}
-		LCD.clear();
-		LCD.drawString("turning", 0, 0);
-		Motor.B.setSpeed(900);
-		Motor.C.setSpeed(900);
-		Motor.B.rotate(turn, true);
-		Motor.C.rotate(turn);
-		Motor.B.flt();
-		Motor.C.flt();
-	}
+            dis.close();
+
+            connection.close();
+            LCD.clear();
+        }
+    }
+
+    public static void turn(boolean b) {
+        int turn = 180;
+        String turning = "Opening";
+        if (!b) {
+            turn = -turn;
+            turning = "Closing";
+        }
+        LCD.clear();
+        LCD.drawString(turning, 0, 0);
+        Motor.B.setSpeed(900);
+        Motor.C.setSpeed(900);
+        Motor.B.rotate(turn, true);
+        Motor.C.rotate(turn);
+        Motor.B.flt();
+        Motor.C.flt();
+    }
 }
--- SlotMachienPC/src/PCMain.java
+++ SlotMachienPC/src/PCMain.java
@@ -6,23 +6,23 @@ import lejos.pc.comm.*;
 
 public class PCMain {
 
-	public static void main(String[] args) throws NXTCommException,
-			IOException, InterruptedException {
+    public static void main(String[] args) throws NXTCommException,
+            IOException, InterruptedException {
 
-		// communicatie opstellen met de brick
-		NXTComm nxtComm = NXTCommFactory.createNXTComm(NXTCommFactory.USB);
-		NXTInfo[] nxtInfo = nxtComm.search(null);
-		nxtComm.open(nxtInfo[0]);
+        // communicatie opstellen met de brick
+        NXTComm nxtComm = NXTCommFactory.createNXTComm(NXTCommFactory.USB);
+        NXTInfo[] nxtInfo = nxtComm.search(null);
+        nxtComm.open(nxtInfo[0]);
 
-		// datastream richting brick opstellen op basis van de stdin
-		OutputStream oStream = nxtComm.getOutputStream();
-		Scanner sc = new Scanner(System.in);
-		while (true) {
-			byte a = sc.nextByte();
-			oStream.write(new byte[] { a });
-			oStream.flush();
+        // datastream richting brick opstellen op basis van de stdin
+        OutputStream oStream = nxtComm.getOutputStream();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            byte a = sc.nextByte();
+            oStream.write(new byte[] { a });
+            oStream.flush();
 
-		}
-	}
+        }
+    }
 
 }
