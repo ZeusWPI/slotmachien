8a471ae06a6c3c3645e990b406fa51e1fda8c365
feliciaan <feliciaan@de-palmenaer.eu>
2014-08-05 17:12:07 +0200

    Added status and updated the webserver a little

--- Web/slotmachien.py
+++ Web/slotmachien.py
@@ -1,7 +1,9 @@
 import settings
 
-from flask import Flask, Blueprint, abort, request
-from flask_peewee.admin import Admin
+from datetime import datetime as dt
+
+from flask import Flask, Blueprint, abort, request, jsonify
+from flask_peewee.admin import Admin, ModelAdmin
 from flask_peewee.auth import Auth
 from flask_peewee.db import Database
 from peewee import *
@@ -22,13 +24,29 @@ db = Database(app)
 
 # Create database models
 class AuthKey(db.Model):
+    service = TextField()
     key = TextField()
+
+    def __unicode__(self):
+        return '%s' % self.service
+
+class AuthKeyAdmin(ModelAdmin):
+    columns = ('service', 'key')
+
+class LogAction(db.Model):
+    auth_key = ForeignKeyField(AuthKey, related_name='actions')
+    action = TextField()
+    logged_on = DateTimeField()
+
+class LogActionAdmin(ModelAdmin):
+    columns = ('auth_key', 'action', 'logged_on',)
 
 # Create an Auth object for use with our flask app and database wrapper
 auth = Auth(app, db)
 
 admin = Admin(app, auth)
-admin.register(AuthKey)
+admin.register(AuthKey, AuthKeyAdmin)
+admin.register(LogAction, LogActionAdmin)
 admin.setup()
 
 slotmachien_bp = Blueprint('slotmachien', __name__)
@@ -36,39 +54,51 @@ slotmachien_bp = Blueprint('slotmachien', __name__)
 
 # Method before request to check if allowed to make request
 def before_slotmachien_request():
-    # Token support for slack, because it doesn't support customizing headers
-    # out of the box
-    auth_key = (request.headers.get('Authorization') or
-                request.args.get('token'))
-    key = AuthKey.select().where(AuthKey.key == auth_key)
-    if key.count() == 0:
+    if not auth_key():
         abort(401)
 slotmachien_bp.before_request(before_slotmachien_request)
 
 
 # Create the routes
-@slotmachien_bp.route("/open", methods=['POST'])
+@slotmachien_bp.route('/open', methods=['POST'])
 def open_door():
-    send_command("open")
-    return "ok"
-
+    return send_command('open')
 
 @slotmachien_bp.route('/close', methods=['POST'])
 def close_door():
-    send_command("close")
-    return "ok"
+    return send_command('close')
+
+@slotmachien_bp.route('/')
+def status_door():
+    return send_command('status')
 
 app.register_blueprint(slotmachien_bp, url_prefix='/slotmachien')
 
 
 def send_command(command):
     # TODO: send command to named pipe
+    log_action(command)
     print(command)
+    # TODO: get response
+    return jsonify({'status': command})
 
+def auth_key():
+    # Token support for slack, because it doesn't support customizing headers
+    # out of the box
+    auth_key = (request.headers.get('Authorization') or
+                request.args.get('token'))
+    try:
+        return AuthKey.get(AuthKey.key == auth_key)
+    except AuthKey.DoesNotExist:
+        return None
+
+def log_action(action):
+    LogAction.create(auth_key=auth_key(), action=action, logged_on=dt.now())
 
 def create():
     auth.User.create_table(fail_silently=True)
     AuthKey.create_table(fail_silently=True)
+    LogAction.create_table(fail_silently=True)
 
 if __name__ == "__main__":
     create()
