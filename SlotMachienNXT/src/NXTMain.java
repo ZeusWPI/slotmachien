c17aec0a02a2004061018226a6cf647f1facb9cf
Jeroen <jeroen.declerck@ugent.be>
2014-08-08 17:28:07 +0200

    sendstatus prototype

--- SlotMachienNXT/src/NXTMain.java
+++ SlotMachienNXT/src/NXTMain.java
@@ -1,164 +1,201 @@
-import java.io.*;
 
-import lejos.nxt.*;
-import lejos.nxt.comm.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+import lejos.nxt.Button;
+import lejos.nxt.ButtonListener;
+import lejos.nxt.LCD;
+import lejos.nxt.Motor;
+import lejos.nxt.Sound;
+import lejos.nxt.comm.NXTConnection;
+import lejos.nxt.comm.USB;
 import lejos.util.Delay;
 
 public class NXTMain {
+
+	private static final int POSITION_OPEN = 0;
+	private static final int POSITION_CLOSED = 180;
+
+	private static final int OPEN_LIMIT_BEFORE_DEADZONE = 40;
+	private static final int CLOSED_LIMIT_BEFORE_DEADZONE = 100;
+
+	private static final int STEPPER_ANGLE = 20;
+	private static final int EXTREMUM_TO_OPEN = -440;
+
+	private static boolean STATUS; // true: door unlocked; False: door locked.
+
+	public static void main(String[] args) throws Exception {
+
+		addButtonListeners();
+
+		// initial calibration
+		calibrate();
+
+		while (true) {
+
+			// Wait for incoming command
+			NXTConnection conn = USB.waitForConnection();
+			DataInputStream dis = conn.openDataInputStream();
+
+			// read and run instruction code
+			byte b = dis.readByte();
+			runCode(b, conn);
+
+			// Terminate incoming connection
+			dis.close();
+			conn.close();
+		}
+
+	}
 	
-    private static final int POSITION_OPEN = 0;
-    private static final int POSITION_CLOSED = 180;
-    
-    private static final int STEPPER_ANGLE = 20;
-    private static final int EXTREMUM_TO_OPEN = -440;
-    
-    public static void main(String[] args) throws Exception {
-        
-        addButtonListeners();
-        
-        // initial calibration
-        calibrate();
-        
-        while (true) {
-            
-            // Wait for incoming command
-            NXTConnection connection = USB.waitForConnection();
-            DataInputStream dis = connection.openDataInputStream();
-            
-            // read and run instruction code
-            byte b = dis.readByte();
-            runCode(b);
-            
-            // Terminate incoming connection
-            dis.close();
-            connection.close();
-        }
+	public static void runCode(byte b, NXTConnection conn) {
+		switch (b) {
+		case 1: // open de door
+			turnTo(POSITION_OPEN);
+			STATUS = true;
+			break;
+		case 2: // close de door
+			turnTo(POSITION_CLOSED);
+			STATUS = false;
+			break;
+		case 3: // send status to slotmachienPC
+			sendStatus(conn);
+			break;
+		}
+		drawString(checkStatus() ? "open" : "closed");
 
-    }
-    
-    public static void runCode(byte b) {
-        switch (b) {
-            case 1: // open de door
-                turnTo(POSITION_OPEN);
-                break;
-            case 2: // close de door
-                turnTo(POSITION_CLOSED);
-                break;
-        }
-        drawString( isOpen()?"open":"closed" );
-        
-    }
+	}
 
-    public static void turn(int turn) {
-        // IK GA HARD MAN
-        Motor.B.setSpeed(900);
-        Motor.C.setSpeed(900);
-        Motor.B.rotate(turn, true);
-        Motor.C.rotate(turn);
-        Motor.B.flt();
-        Motor.C.flt();
-    }
-    
-    // CODE DUPLICATION !!!1!!
-    public static void turnTo(int turn) {
-        // IK GA HARD MAN
-        Motor.B.setSpeed(900);
-        Motor.C.setSpeed(900);
-        Motor.B.rotateTo(turn, true);
-        Motor.C.rotateTo(turn);
-        Motor.B.flt();
-        Motor.C.flt();
-    }
+	public static void turn(int turn) {
+		// IK GA HARD MAN
+		Motor.B.setSpeed(900);
+		Motor.C.setSpeed(900);
+		Motor.B.rotate(turn, true);
+		Motor.C.rotate(turn);
+		Motor.B.flt();
+		Motor.C.flt();
+	}
 
-    private static final int OPEN_LIMIT = 40;
-    private static final int CLOSED_LIMIT = 100;
+	// CODE DUPLICATION !!!1!!
+	public static void turnTo(int turn) {
+		// IK GA HARD MAN
+		Motor.B.setSpeed(900);
+		Motor.C.setSpeed(900);
+		Motor.B.rotateTo(turn, true);
+		Motor.C.rotateTo(turn);
+		Motor.B.flt();
+		Motor.C.flt();
+	}
 
-    // Get door status    
-    public static boolean isOpen() {
-        if (  Motor.B.getTachoCount() < OPEN_LIMIT ) {
-            return true;
-        }
-        if ( Motor.B.getTachoCount() > CLOSED_LIMIT) {
-            return false;
-        }
-        
-        // If no status could be determined, recalibrate (aka shit's fucked, yo).
-        calibrate();
-        return true;
-    }
+	// check door status
+	public static boolean checkStatus() {
+		if (Motor.B.getTachoCount() < OPEN_LIMIT_BEFORE_DEADZONE
+				&& STATUS == true) {
+			return true;
+		}
+		if (Motor.B.getTachoCount() > CLOSED_LIMIT_BEFORE_DEADZONE
+				&& STATUS == false) {
+			return false;
+		}
 
-    public static void drawString(String s) {
-        LCD.clear();
-        LCD.drawString(s, 0, 0);
-    }
+		// If no status could be determined, recalibrate because
+		// the physical lock angle is located in the deadzone, or
+		// the current STATUS does not match the physical lock status
+		calibrate();
+		return true;
+	}
 
-    public static void calibrate() {
-        drawString("calibrating");
-        // Turn to extremum as calibration point (in steps of 20)
-        Motor.B.setStallThreshold(5, 500);
-        while(!Motor.B.isStalled()) {
-            turn(STEPPER_ANGLE);
-        }
-        // Turn to open position
-        turn(EXTREMUM_TO_OPEN); 
-        
-        // Calibrate open position to 0
-        Delay.msDelay(200);
-        Motor.B.resetTachoCount();
-        Motor.C.resetTachoCount();
-        
-        //shit's fucked, yo
-        Motor.B.flt();
-        Motor.C.flt();
-        
-        drawString("calibrated");
-    }
-    
-    public static void addButtonListeners() {
-        Button.LEFT.addButtonListener(new ButtonListener() {
-            public void buttonPressed(Button b) {
-                turnTo(POSITION_OPEN);
-            }
+	public static void drawString(String s) {
+		LCD.clear();
+		LCD.drawString(s, 0, 0);
+	}
 
-            public void buttonReleased(Button b) {
-                drawString( isOpen()?"open":"closed" );
-            }
-        });
-        Button.RIGHT.addButtonListener(new ButtonListener() {
-            public void buttonPressed(Button b) {
-                turnTo(POSITION_CLOSED);
-            }
+	public static void calibrate() {
+		drawString("calibrating");
+		// Turn to extremum as calibration point (in steps of 20)
+		Motor.B.setStallThreshold(5, 500);
+		while (!Motor.B.isStalled()) {
+			turn(STEPPER_ANGLE);
+		}
+		// Turn to open position
+		turn(EXTREMUM_TO_OPEN);
 
-            public void buttonReleased(Button b) {
-                drawString( isOpen()?"open":"closed" );
-            }
-        });
-        Button.ENTER.addButtonListener(new ButtonListener() {
-            public void buttonPressed(Button b) {
-                //MAAK KABAAL LOL
-                drawString("LOLOLOLOLOLOLOL");
-                File muziekje = new File("muziekje.wav");
-                Sound.playSample(muziekje, 100);
-                Delay.msDelay(12000);
-                drawString( isOpen()?"open":"closed" );
-            }
+		// Calibrate open position to 0
+		Delay.msDelay(200);
+		Motor.B.resetTachoCount();
+		Motor.C.resetTachoCount();
 
-            public void buttonReleased(Button b) {
-                //nothing lol
-            }
-        });
-        Button.ESCAPE.addButtonListener(new ButtonListener() {
-            public void buttonPressed(Button b) {
-                //TO BE REPLACED WITH "calibrate()" BEFORE DEPLOYMENT
-                //CURRENTLY FOR DEBUGGING ONLY
-                
-                System.exit(0);
-            }
+		// shit's fucked, yo
+		Motor.B.flt();
+		Motor.C.flt();
 
-            public void buttonReleased(Button b) {
-                //nothing lol
-            }
-        });
-        
-    }
+		STATUS = true;
+
+		drawString("calibrated");
+	}
+
+	public static void sendStatus(NXTConnection conn) {
+		DataOutputStream dos = conn.openDataOutputStream();
+		byte b = 0;
+		if (!STATUS) {
+			b = 1;
+		}
+		try {
+			dos.write(b);
+			dos.flush();
+			dos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void addButtonListeners() {
+		Button.LEFT.addButtonListener(new ButtonListener() {
+			public void buttonPressed(Button b) {
+				runCode((byte) 1, null); //I highly doubt this is good practice
+			}
+
+			public void buttonReleased(Button b) {
+				drawString(checkStatus() ? "open" : "closed");
+			}
+		});
+		Button.RIGHT.addButtonListener(new ButtonListener() {
+			public void buttonPressed(Button b) {
+				runCode((byte) 2, null); //I highly doubt this is good practice
+			}
+
+			public void buttonReleased(Button b) {
+				drawString(checkStatus() ? "open" : "closed");
+			}
+		});
+		Button.ENTER.addButtonListener(new ButtonListener() {
+			public void buttonPressed(Button b) {
+				// MAAK KABAAL LOL
+				drawString("LOLOLOLOLOLOLOL");
+				File muziekje = new File("muziekje.wav");
+				Sound.playSample(muziekje, 100);
+				Delay.msDelay(12000);
+				drawString(checkStatus() ? "open" : "closed");
+			}
+
+			public void buttonReleased(Button b) {
+				// nothing lol
+			}
+		});
+		Button.ESCAPE.addButtonListener(new ButtonListener() {
+			public void buttonPressed(Button b) {
+				// TO BE REPLACED WITH "calibrate()" BEFORE DEPLOYMENT
+				// CURRENTLY FOR DEBUGGING ONLY
+
+				System.exit(0);
+			}
+
+			public void buttonReleased(Button b) {
+				// nothing lol
+			}
+		});
+
+	}
 }
--- SlotMachienPC/src/PCMain.java
+++ SlotMachienPC/src/PCMain.java
@@ -1,8 +1,8 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 import lejos.pc.comm.NXTComm;
 import lejos.pc.comm.NXTCommException;
@@ -11,46 +11,47 @@ import lejos.pc.comm.NXTInfo;
 
 public class PCMain {
 
-    private static final Map<String, Byte> TOLK = new HashMap<String, Byte>() {
+	private static final Map<String, Byte> TOLK = new HashMap<String, Byte>() {
 
-        private static final long serialVersionUID = 1L;
+		private static final long serialVersionUID = 1L;
 
-        {
-            put("open", (byte) 1);
-            put("close", (byte) 2);
-            put("restart", (byte) 3);
-            put("test", (byte) 4);
-            put("clear", (byte) 5);
-            put("quit", (byte) 6);
-        }
-    };
+		{
+			put("open", (byte) 1);
+			put("close", (byte) 2);
+			put("status", (byte) 3);
+		}
+	};
 
-    public static void main(String[] args) throws NXTCommException,
-            IOException, InterruptedException {
+	public static void main(String[] args) throws NXTCommException,
+			IOException, InterruptedException {
 
-        // communicatie opstellen met de brick
-        NXTComm nxtComm = NXTCommFactory.createNXTComm(NXTCommFactory.USB);
-        NXTInfo[] nxtInfo = nxtComm.search(null);
-        nxtComm.open(nxtInfo[0]);
-        try {
-                // datastream richting brick opstellen op basis van de stdin
-                OutputStream oStream = nxtComm.getOutputStream();
-            try {
-                String commando = args[0].toLowerCase();
-                if (TOLK.containsKey(commando)) {
-                    oStream.write(TOLK.get(commando));
-                    System.out.print(commando);
-                    oStream.flush();
-                }
-            } catch (Exception ex) {
+		// communicatie opstellen met de brick
+		NXTComm nxtComm = NXTCommFactory.createNXTComm(NXTCommFactory.USB);
+		NXTInfo[] nxtInfo = nxtComm.search(null);
+		nxtComm.open(nxtInfo[0]);
+		try {
+			// datastream richting brick opstellen op basis van de stdin
+			OutputStream oStream = nxtComm.getOutputStream();
+			try {
+				String commando = args[0].toLowerCase();
+				if (TOLK.containsKey(commando)) {
+					oStream.write(TOLK.get(commando));
+					if (TOLK.get(commando) == 3) {
+						InputStream iStream = nxtComm.getInputStream();
+						System.out.print((byte) iStream.read());
+						iStream.close();
+					}
+					oStream.flush();
+				}
+			} catch (Exception ex) {
 
-            } finally {
-                oStream.close();
-            }
-        } catch (Exception ex) {
+			} finally {
+				oStream.close();
+			}
+		} catch (Exception ex) {
 
-        } finally {
-            nxtComm.close();
-        }
-    }
+		} finally {
+			nxtComm.close();
+		}
+	}
 }
