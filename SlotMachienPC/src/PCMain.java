ea58c400b62b7b80c2dc29cfeb4e83f3757931f9
Lorin Werthen <donvittoriomusic@gmail.com>
2015-02-03 16:15:34 +0100

    kill me

--- SlotMachienPC/src/PCMain.java
+++ SlotMachienPC/src/PCMain.java
@@ -22,7 +22,7 @@ public class PCMain {
         OutputStream os = nxtComm.getOutputStream();
         InputStream is = nxtComm.getInputStream();
         
-        nxtComm.open(nxtInfo);
+        nxtComm.open(nxtInfo[0]);
 
         Pipe.make(is, System.out);
         Pipe.make(System.in, os).join();  // Wait for end of input
